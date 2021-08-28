623bf53a63b4c4fa44460c56de78abbe6b7b89ec
Oscar Torres <oscardtorres@hotmail.com>
2021-08-28 01:12:04 -0500

    changed database settings

--- config.ts
+++ config.ts
@@ -1,6 +1,13 @@
-export const database = {
+/* export const database = {
   username: "root",
   password: "MySql304!",
   database: "elcampin",
   host: "localhost"
+} */
+
+export const database = {
+  username: "b82628ff7be753",
+  password: "e47ff5c9",
+  database: "heroku_4d96424581c5c29",
+  host: "us-cdbr-east-04.cleardb.com"
 }
