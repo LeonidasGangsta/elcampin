1b27ea8eb8e994b108738cc670938a739a2333ea
Oscar Torres <oscardtorres@hotmail.com>
2021-08-28 02:13:24 -0500

    removed slash on url

--- app/client/src/utils/api/httpHelper.ts
+++ app/client/src/utils/api/httpHelper.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import axios, { AxiosResponse } from 'axios';
 
-const API_URL = 'https://elcampin-api.herokuapp.com/';
+const API_URL = 'https://elcampin-api.herokuapp.com';
 
 const dataExtractor = (axiosPromise: AxiosResponse) => ({
   data: axiosPromise.data,
