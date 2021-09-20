1ce191ae9b146243e9b2a29a59c8bc26da762d8f
Oscar Torres <oscardtorres@hotmail.com>
2021-09-19 20:36:42 -0500

    Removed old material ui packages

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CssBaseline, ThemeProvider, StyledEngineProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider, Theme, StyledEngineProvider } from '@mui/material';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import theme from './styles/theme';
 import './index.css';
+
+declare module '@mui/styles/defaultTheme' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface DefaultTheme extends Theme {}
+}
 
 ReactDOM.render(
   <React.StrictMode>
